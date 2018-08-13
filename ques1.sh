277c49d62825619dcd149dd2ebe630b0b3581a29
vaish-agarwal <42140142+vaish-agarwal@users.noreply.github.com>
2018-08-13 15:08:20 +0530

    Add files via upload

---
+++ ques1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "what is your name?"
+
+read NAME
+
+echo $NAME
+
+mkdir $NAME
+
+cd $NAME
+
+touch $NAME.cpp
+
---
+++ ques2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "what is your name?"
+
+read NAME
+
+echo 
+
+mkdir $NAME
+
+cd $NAME
+
+touch "$NAME 1.cpp"
+
+touch "$NAME 2.cpp"
+
+touch "$NAME 3.cpp"
+
+touch "$NAME 4.cpp"
+
+touch "$NAME 5.cpp"
