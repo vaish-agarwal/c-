096aecd607365fe2f0db743d02b6b66a4e14cda8
vaish-agarwal <42140142+vaish-agarwal@users.noreply.github.com>
2018-08-16 20:37:01 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,26 @@
+//include library
+#include<iostream>
+using namespace std;
+int main(){
+cout <<"hello world" <<endl;
+
+//declare all the variables
+int value;
+int a,b;
+cout <<"size of a="<<sizeof(a) <<endl;
+cout<<"size of b="<<sizeof(b) <<endl;
+
+float sum=2+1.7;
+cout<< sum<<endl;
+cout<<"size of sum="<<sizeof (sum) <<endl;
+
+char z[]="my name is vaishali.";
+cout<<z <<endl;
+cout <<"size of z="<<sizeof (z)<<endl;
+
+bool Iamhappywiththenewcsclasstimings=false;
+cout<<Iamhappywiththenewcsclasstimings<<endl;
+
+
+ 
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=11 ,b=63;
+int sum=a+b ;
+cout<<sum<<endl;
+int difference=a-b;
+cout<<difference<<endl;
+int product=a*b;
+cout<<product<<endl; 
+int division=b/a;
+cout<<division<<endl;
+int mod=b%a;
+cout<<mod<<endl;
+}
+
