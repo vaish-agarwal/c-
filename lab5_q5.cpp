3e326fcbb1081dfdfbda82797afbc794879763ec
vaish-agarwal <42140142+vaish-agarwal@users.noreply.github.com>
2018-09-05 17:56:45 +0530

    Add files via upload

---
+++ lab5_q5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+//declare the variables
+int a,b,c;
+
+//ask 3 sides of a triangle 
+cout<<"enter 3 sides of a triangle"<<endl;
+
+//store in variables
+cin>>a>>b>>c;
+
+//check whether it exists or not
+if(a+b>=c && b+c>=a && c+a>=b){
+cout<<"it is a triangle";
+}
+else {
+cout<<"it is not a triangle";
+
+}
+//display the output
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
