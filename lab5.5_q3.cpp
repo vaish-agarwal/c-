d986167f53622215eb48237ed5e357d3b5299274
vaish-agarwal <42140142+vaish-agarwal@users.noreply.github.com>
2018-09-06 20:27:57 +0530

    Update lab5.5_q3.cpp

--- lab5.5_q3.cpp
+++ lab5.5_q3.cpp
@@ -10,18 +10,18 @@ cout<<"rhombus star pattern"<<endl;
 *****
 
 */
-//print spaces in rows
-for(int i = 0; i <5; i++){
+   //print spaces in rows
+   for(int i = 0; i <5; i++){
 
-//print spaces
-for(int j = 0; j <4-i; j++){
-cout<<" ";
-}
- 
-//print stars
-for(int a = 0; a <5; a++){
-cout<<"*";
-}
-cout<<endl;
-}
+      //print spaces
+      for(int j = 0; j <4-i; j++){
+      cout<<" ";
+      }
+
+         //print stars
+         for(int a = 0; a <5; a++){
+         cout<<"*";
+         }
+      cout<<endl;
+   }
 }
