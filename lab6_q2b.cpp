7e97d0080e7ab181ac14586bdcfe4a160f48a640
vaish-agarwal <42140142+vaish-agarwal@users.noreply.github.com>
2018-09-08 12:15:17 +0530

    Update lab6_q2b.cpp

--- lab6_q2b.cpp
+++ lab6_q2b.cpp
@@ -3,26 +3,21 @@ using namespace std;
 
 
 
-/*(By Reference) Goal is the same as above, but this time, the function that adds the numbers should be void,and takes a third,*/
+/*(By Reference) Goal is the same as above, but this time, the function that adds the numbers should be void,and takes a third,(By Reference) Goal is the same as above, but this time, the function that finds the maximum should be void, and takes a third, pass by reference parameter; then puts the maximum in that.
+*/
 void sum(int a, int b , int &result){
 result = a+b ;
 
 
 }
 
+    //  calls the function and display the result
+    int main(){
+    int a,b,result;
+    cout<<"enter numbers."<<endl;
+    cin>>a>>b;
+    sum(a,b,result);
+    cout<<"SUM:"<< result;
 
 
-
-
-//  pass by reference parameter; then puts the sum in that.
-
-
-int main(){
-int a,b,result;
-cout<<"enter numbers."<<endl;
-cin>>a>>b;
-sum(a,b,result);
-cout<<"SUM:"<< result;
-
-
-}
+    }
