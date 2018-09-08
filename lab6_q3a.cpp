bf2d933d611b682bb68f39e9230bd17290bab681
vaish-agarwal <42140142+vaish-agarwal@users.noreply.github.com>
2018-09-08 12:16:36 +0530

    Update lab6_q3a.cpp

--- lab6_q3a.cpp
+++ lab6_q3a.cpp
@@ -6,32 +6,24 @@ using namespace std;
 /*(By Value) Write a program with a function that takes two int parameters, finds the maximum, then returns the maximum.*/
 int max(int a, int b, int max1){
 
-if(a>b){
-max1 = a;
+  if(a>b){
+  max1 = a;
+  }
+  else if(a == b){
+  max1 = a;
+  }
+  else{
+  max1 = b;
+  }
+  return max1;  
 }
-else if(a == b){
-max1 = a;
-}
-else{
-max1 = b;
-}
-return max1;  
-}
 
+    /* The program should ask the user for two numbers, then call the function with the numbers as arguments, and tell the user the maximum.
+    */
+    int main(){
+    int a,b, max1;
+    cout<<"enter numbers"<<endl;
+    cin>>a>>b;
+    cout<<"MAX:"<< max(a,b);
 
-
-
-
-
-
-/* The program should ask the user for two numbers, then call the function with the numbers as arguments, and tell the user the maximum.
-*/
-
-
-int main(){
-int a,b, max1;
-cout<<"enter numbers"<<endl;
-cin>>a>>b;
-cout<<"MAX:"<< max(a,b);
-
-}
+    }
